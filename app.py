aeb1f28133b25729e685837ccd9e84dfe11c829a
thiagonery <60719065+thiagonery@users.noreply.github.com>
2022-01-29 12:51:19 -0300

    Commit erro test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World - Thiago Nery"
+    return "Hello World - Thiago B Nery"
 
 if __name__ == '__main__':
     app.run()
